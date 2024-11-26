9fab6f642c997ae4b3e4e94d049fa1c5aa4f28bd
pratikgupta449 <138274656+pratikgupta449@users.noreply.github.com>
2024-11-26 17:18:41 +0530

    Update CategoryService.java

--- src/main/java/com/ty/Service/CategoryService.java
+++ src/main/java/com/ty/Service/CategoryService.java
@@ -15,22 +15,22 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository categoryRepository;
 
-	// Get all categories with pagination
+	
 	public Iterable<Category> getAllCategories(int page) {
 		return categoryRepository.findAll(PageRequest.of(page, 10)); // Page size of 10
 	}
 
-	// Create a new category
+	
 	public Category createCategory(Category category) {
 		return (Category) categoryRepository.save(category);
 	}
 
-	// Get category by ID
+	
 	public Optional<Category> getCategoryById(Long id) {
 		return categoryRepository.findById(id);
 	}
 
-	// Update category by ID
+	
 	public Object updateCategory(Long id, Category category) {
 		Optional<Category> existingCategory = categoryRepository.findById(id);
 		if (existingCategory.isPresent()) {
@@ -40,7 +40,7 @@ public class CategoryService {
 		return null;
 	}
 
-	// Delete category by ID
+	
 	public boolean deleteCategory(Long id) {
 		if (categoryRepository.existsById(id)) {
 			categoryRepository.deleteById(id);
